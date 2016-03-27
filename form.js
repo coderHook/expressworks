32cc937af22e2550d858f9ae453faf07232a867e
CoderHooK <Abel1987@hotmail.es>
2016-03-28 01:10:12 +0200

    Solution to Gold Old Form expressworks FCC Challenges

---
+++ form.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var app = express();
+
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.post('/form', function(req, res){
+
+  var myform = req.body.str.split('').reverse().join('');
+
+  res.end(myform);
+});
+
+app.listen(process.argv[2]);
